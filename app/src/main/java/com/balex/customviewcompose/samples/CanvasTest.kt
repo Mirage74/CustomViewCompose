deae6c07ba9962e9adeece3fcb07b5e454e48e43
balex <balexvicx@gmail.com>
2024-05-27 00:08:50 +0200

    l1

--- app/src/main/java/com/balex/customviewcompose/samples/CanvasTest.kt
+++ app/src/main/java/com/balex/customviewcompose/samples/CanvasTest.kt
@@ -8,8 +8,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
+@Preview
 @Composable
 fun CanvasTest() {
     Canvas(
